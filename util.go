8b3626308f54f32b00cfb1a361f75be8dd575edb
Scott Moser <smoser@brickies.net>
2020-02-05 11:56:23 -0500

    findRangeGaps: fix bug if input was not sorted.
    findRangeGaps assumed sorted input (by start).
    This caused a bug when summing available space as walking
    the PartitionSet is not guaranteed order.

--- util.go
+++ util.go
@@ -1,6 +1,9 @@
 package disko
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 type uRange struct {
 	Start, End uint64
@@ -10,13 +13,22 @@ func (r *uRange) Size() uint64 {
 	return r.End - r.Start
 }
 
+type uRanges []uRange
+
+func (r uRanges) Len() int           { return len(r) }
+func (r uRanges) Less(i, j int) bool { return r[i].Start < r[j].Start }
+func (r uRanges) Swap(i, j int)      { r[i], r[j] = r[j], r[i] }
+
 // findRangeGaps returns a set of uRange to represent the un-used
 // uint64 between min and max that are not included in ranges.
 //  findRangeGaps({{10, 40}, {50, 100}}, 0, 110}) ==
 //      {{0, 9}, {41, 49}, {101, 110}}
-func findRangeGaps(ranges []uRange, min, max uint64) []uRange {
+// Note that input list will be sorted.
+func findRangeGaps(ranges uRanges, min, max uint64) uRanges {
 	// start 'ret' off with full range of min to max, then start cutting it up.
-	ret := []uRange{{min, max}}
+	ret := uRanges{{min, max}}
+
+	sort.Sort(ranges)
 
 	for _, i := range ranges {
 		for r := 0; r < len(ret); r++ {
