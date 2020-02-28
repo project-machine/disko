7a11cf251368f55dd5df9476ded890d8607d561a
Scott Moser <scmoser@cisco.com>
2020-02-27 22:20:13 -0500

    Add lvm subcommand and dump-vgs.
    dump-vgs will do a scan and dump the data that disko read.

---
+++ demo/lvm.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/anuvu/disko"
+	"github.com/anuvu/disko/linux"
+	"github.com/urfave/cli/v2"
+)
+
+//nolint:gochecknoglobals
+var lvmCommands = cli.Command{
+	Name:  "lvm",
+	Usage: "lvm commands",
+	Subcommands: []*cli.Command{
+		{
+			Name:   "dump-vgs",
+			Usage:  "Scan system and dump disko VGs.  Optionally give a vg name.",
+			Action: lvmDumpVGs,
+		},
+	},
+}
+
+func lvmDumpVGs(c *cli.Context) error {
+	var filter disko.VGFilter
+
+	if c.Args().Len() == 0 {
+		filter = func(v disko.VG) bool { return true }
+	} else if c.Args().Len() == 1 {
+		filter = func(v disko.VG) bool { return v.Name == c.Args().First() }
+	} else {
+		return fmt.Errorf("too many args. Really just want 1. Got %d", c.Args().Len())
+	}
+
+	vmgr := linux.VolumeManager()
+
+	vgset, err := vmgr.ScanVGs(filter)
+	if err != nil {
+		return err
+	}
+
+	jbytes, err := json.MarshalIndent(&vgset, "", "  ")
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(string(jbytes))
+
+	return nil
+}
--- demo/main.go
+++ demo/main.go
@@ -14,6 +14,7 @@ func main() {
 		Commands: []*cli.Command{
 			&diskCommands,
 			&megaraidCommands,
+			&lvmCommands,
 		},
 	}
 
