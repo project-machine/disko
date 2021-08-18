874b4852d1a8ee3cfb936476fb8f84a9dd13bd6c
Scott Moser <scmoser@cisco.com>
2021-08-17 21:30:54 -0400

    luks 2 support

--- linux/lvm.go
+++ linux/lvm.go
@@ -429,12 +429,13 @@ func getLuksInfo(devpath string) (bool, string, string, error) {
 	} else if rc != 0 {
 		return crypt, "", "", cmdError(cmd, stdout, stderr, rc)
 	}
+	// prefix looks like CRYPT-LUKS[12]-<luksUUID-without-spaces>-
+	bareID := strings.ReplaceAll(string(chompBytes(stdout)), "-", "")
+	luks1 := "CRYPT-LUKS1-" + bareID + "-"
+	luks2 := "CRYPT-LUKS2-" + bareID + "-"
 
 	crypt = true
 	minFields := 4
-	// prefix looks like CRYPT-LUKS1-<luksUUID-without-spaces>-
-	prefix := "CRYPT-LUKS1-" +
-		strings.ReplaceAll(string(chompBytes(stdout)), "-", "") + "-"
 
 	cmd = []string{"dmsetup", "table", "--concise"}
 	stdout, stderr, rc = runCommandWithOutputErrorRc(cmd...)
@@ -455,7 +456,7 @@ func getLuksInfo(devpath string) (bool, string, string, error) {
 					len(fields), minFields, record)
 		}
 
-		if strings.HasPrefix(fields[1], prefix) {
+		if strings.HasPrefix(fields[1], luks1) || strings.HasPrefix(fields[1], luks2) {
 			return crypt, fields[0], "/dev/mapper/" + fields[0], nil
 		}
 	}
